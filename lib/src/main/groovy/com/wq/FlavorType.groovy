c98c02229c4c7292e7c24d9064be4ed59bd580b3
wangqi <wangqi_vivian@sina.com>
2020-05-26 11:22:34 +0800

    app差异化打包功能

---
+++ lib/src/main/groovy/com/wq/FlavorType.groovy
@@ -0,0 +1,12 @@
+package com.wq
+
+public class FlavorType {
+    /**
+     * 存放渠道包图片的路径
+     */
+    String resourceDir
+    /**
+     * 主项目名
+     */
+    String appName
+}
---
+++ lib/src/main/groovy/com/wq/ResourceFlavorsPlugin.groovy
@@ -0,0 +1,48 @@
+package com.atom
+
+import com.wq.FlavorType
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.Task
+
+public class ResourceFlavorsPlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+
+        project.extensions.add("rfp", FlavorType)
+        project.afterEvaluate {
+            FlavorType ext = project.rfp
+            def resourceDir = ext.resourceDir
+            def appName = ext.appName
+            //非打包时不做任何操作
+            def startParameter = project.gradle.startParameter
+            def targetTasks = startParameter.taskNames
+
+            targetTasks.each {
+                if (!it.contains("assemble") && !it.contains("aR")) {
+                    return
+                }
+            }
+
+            project.android.applicationVariants.all { variant ->
+                String variantName = variant.name.capitalize()
+                def variantFlavorName = variant.flavorName
+                Task preBuild = project.tasks["pre${variantName}Build"]
+                if (variantFlavorName == null || "" == variantFlavorName) {
+                    return
+                }
+                preBuild.doFirst {
+                    project.copy {
+                        from "../${resourceDir}/${variantFlavorName}"
+                        into "../${appName}/src/main/res"
+                    }
+                    println "../${resourceDir}/${variantFlavorName}"
+                    println "../${appName}/src/main/res"
+                    if (fworkResult.getDidWork())
+                        println "${variantFlavorName} resource is changed!"
+                }
+            }
+        }
+    }
+}
